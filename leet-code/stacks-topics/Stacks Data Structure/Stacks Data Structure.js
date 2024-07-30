387381e3eb28c87833988e152d83d2c2d79f5ab0
Puneeth1996 <puneeth1996p@gmail.com>
2024-07-30 12:20:23 +0530

    Stacks prototype in javascript

---
+++ leet-code/stacks-topics/Stacks Data Structure/Stacks Data Structure.js
@@ -0,0 +1,44 @@
+// stacks & ques are done with Arrays
+// Push and pop are T(1) and O(1) is constant time & space
+// Enque and deque for ques
+class Node {
+	constructor(value) {
+		this.value = value
+		this.next = null
+	}
+}
+//LIFO
+//add at beg
+//remove from the beg
+class Stack {
+	constructor() {
+		this.first = null
+		this.last = null
+		this.size = 0
+	}
+	addAtBeginning(value) {
+		const node = new Node(value)
+		if (!this.first) {
+			this.first = node
+			this.last = node
+		} else {
+			let temp = this.first
+			this.first = node
+			this.first.next = temp
+		}
+		this.size++
+		return this
+	}
+	removeFromBeginning() {
+		if (!this.first) return null
+		let temp = this.first
+		this.first = this.first.next
+		this.size--
+		if (this.size === 0) {
+			this.last = null
+		}
+		return temp
+	}
+}
+
+const stack = new Stack()
