2b004066e6a73b81b34fc75137f9f7f5c813f817
Puneeth1996 <puneeth1996p@gmail.com>
2024-08-26 23:03:36 +0530

    1002. Find Common Characters , Time and space complexit

--- leet-code/strings-topics/1002. Find Common Characters/1002. Find Common Characters.js
+++ leet-code/strings-topics/1002. Find Common Characters/1002. Find Common Characters.js
@@ -2,6 +2,9 @@
  * @param {string[]} A
  * @return {string[]}
  */
+
+// Time:O(Σ∣words[i]∣)
+// Space:O(max(∣words[i]∣))
 const commonChars = (A) => {
 	const commonArr = []
 	const firstWordArr = A[0].split('')
