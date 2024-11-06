9df3a8385b9b5f4668fcb4de0ce9cab1741dc12a
Puneeth1996 <puneeth1996p@gmail.com>
2024-11-06 13:25:37 +0530

    3330. Find the Original Typed String I

--- leet-code/strings-topics/3330. Find the Original Typed String I/3330. Find the Original Typed String I.js
+++ leet-code/strings-topics/3330. Find the Original Typed String I/3330. Find the Original Typed String I.js
@@ -1,23 +1,37 @@
+// /**
+//  * @param {string} word
+//  * @return {number}
+//  */
+// var possibleStringCount = function (word) {
+// 	wordLength = word.length
+// 	if (1 <= wordLength <= 100) {
+// 		let count = 1
+// 		let wordFreq = {}
+// 		for (let i = 0; i < wordLength; i++) {
+// 			if (word[i] in wordFreq) {
+// 				wordFreq[word[i]] += 1
+// 				if (i > 0 && wordFreq[word[i]] === wordFreq[word[i - 1]]) {
+// 					count += 1
+// 				}
+// 			} else {
+// 				wordFreq[word[i]] = 1
+// 			}
+// 		}
+// 		return count
+// 	}
+// 	return 'Constraints Violated'
+// }
+
 /**
  * @param {string} word
  * @return {number}
  */
 var possibleStringCount = function (word) {
-	wordLength = word.length
-	if (1 <= wordLength <= 100) {
-		let count = 1
-		let wordFreq = {}
-		for (let i = 0; i < wordLength; i++) {
-			if (word[i] in wordFreq) {
-				wordFreq[word[i]] += 1
-				count += 1
-			} else {
-				wordFreq[word[i]] = 1
-			}
-		}
-		return count
+	let f = 1
+	for (let i = 1; i < word.length; ++i) {
+		f += word[i] === word[i - 1] ? 1 : 0
 	}
-	return 'Constraints Violated'
+	return f
 }
 
 // Other solution thoughts
