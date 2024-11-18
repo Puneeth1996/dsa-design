f109f0efcd582b6e8550a5426311bef949c1ad02
Puneeth1996 <puneeth1996p@gmail.com>
2024-11-18 22:28:55 +0530

    1422. Maximum Score After Splitting a String

---
+++ leet-code/strings-topics/1422. Maximum Score After Splitting a String/1422. Maximum Score After Splitting a String.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxScore = function (s) {
+	let [l, r] = [0, 0]
+	for (const c of s) {
+		r += c === '1' ? 1 : 0
+	}
+	let ans = 0
+	for (let i = 0; i < s.length - 1; ++i) {
+		if (s[i] === '0') {
+			++l
+		} else {
+			--r
+		}
+		ans = Math.max(ans, l + r)
+	}
+	return ans
+}
