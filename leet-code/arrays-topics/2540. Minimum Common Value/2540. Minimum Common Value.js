b2c370a5275a2c53ec48d7160703e88355c514c8
Puneeth1996 <puneeth1996p@gmail.com>
2024-11-27 22:09:36 +0530

    2540. Minimum Common Value

---
+++ leet-code/arrays-topics/2540. Minimum Common Value/2540. Minimum Common Value.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var getCommon = function (nums1, nums2) {
+	const m = nums1.length
+	const n = nums2.length
+	let i = 0
+	let j = 0
+	while (i < m && j < n) {
+		if (nums1[i] === nums2[j]) {
+			return nums1[i]
+		}
+		if (nums1[i] < nums2[j]) {
+			i++
+		} else {
+			j++
+		}
+	}
+	return -1
+}
