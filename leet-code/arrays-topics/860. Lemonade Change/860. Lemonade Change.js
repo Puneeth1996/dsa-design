f20900db695ae0c06647d611991919813eafb7e2
Puneeth1996 <puneeth1996p@gmail.com>
2024-12-10 22:27:10 +0530

    860. Lemonade Change

---
+++ leet-code/arrays-topics/860. Lemonade Change/860. Lemonade Change.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function (bills) {
+	let [five, ten] = [0, 0]
+	for (const x of bills) {
+		switch (x) {
+			case 5:
+				five++
+				break
+			case 10:
+				five--
+				ten++
+				break
+			case 20:
+				if (ten) {
+					ten--
+					five--
+				} else {
+					five -= 3
+				}
+				break
+		}
+
+		if (five < 0) {
+			return false
+		}
+	}
+	return true
+}
