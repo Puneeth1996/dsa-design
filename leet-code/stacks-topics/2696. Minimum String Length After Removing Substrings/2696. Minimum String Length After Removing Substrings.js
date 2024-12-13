05c3fa2c1114af6cc050ad74fbb447d3d0cb7326
Puneeth1996 <puneeth1996p@gmail.com>
2024-12-13 11:08:24 +0530

    2696. Minimum String Length After Removing Substrings

---
+++ leet-code/stacks-topics/2696. Minimum String Length After Removing Substrings/2696. Minimum String Length After Removing Substrings.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minLength = function (s) {
+	const stk = []
+	for (const c of s) {
+		if (
+			(stk.at(-1) === 'A' && c === 'B') ||
+			(stk.at(-1) === 'C' && c === 'D')
+		) {
+			stk.pop()
+		} else {
+			stk.push(c)
+		}
+	}
+	return stk.length
+}
