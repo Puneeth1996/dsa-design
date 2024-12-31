c9895c60dd80efa1d776d20b75c3d744874774b0
Puneeth1996 <puneeth1996p@gmail.com>
2024-12-31 21:06:50 +0530

    1572. Matrix Diagonal Sum

---
+++ leet-code/arrays-topics/1572. Matrix Diagonal Sum/1572. Matrix Diagonal Sum.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function (mat) {
+	let ans = 0
+	const n = mat.length
+	for (let i = 0; i < n; ++i) {
+		const j = n - i - 1
+		ans += mat[i][i] + (i === j ? 0 : mat[i][j])
+	}
+	return ans
+}
