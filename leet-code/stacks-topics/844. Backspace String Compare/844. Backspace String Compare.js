5ed8686d05e0ab5e83d1ddfe4f250a99447d780b
Puneeth1996 <puneeth1996p@gmail.com>
2025-01-08 17:02:08 +0530

    844. Backspace String Compare | Recurrsion patttern

--- leet-code/stacks-topics/844. Backspace String Compare/844. Backspace String Compare.js
+++ leet-code/stacks-topics/844. Backspace String Compare/844. Backspace String Compare.js
@@ -3,44 +3,34 @@
  * @param {string} t
  * @return {boolean}
  */
+
+function nextValidChar(str, index) {
+	let backspaceSkips = 0
+	while (index >= 0) {
+		if (str[index] === '#') {
+			backspaceSkips++
+		} else if (backspaceSkips > 0) {
+			backspaceSkips--
+		} else {
+			break
+		}
+		index--
+	}
+	return index
+}
+
 var backspaceCompare = function (S, T) {
 	let pointerS = S.length - 1
 	let pointerT = T.length - 1
 
 	while (pointerS >= 0 || pointerT >= 0) {
-		let skipS = 0
-		while (pointerS >= 0) {
-			if (S[pointerS] === '#') {
-				skipS++
-			} else if (skipS > 0) {
-				skipS--
-			} else {
-				break
-			}
-			pointerS--
-		}
-
-		let skipT = 0
-
-		while (pointerT >= 0) {
-			if (T[pointerT] === '#') {
-				skipT++
-			} else if (skipT > 0) {
-				skipT--
-			} else {
-				break
-			}
-			pointerT--
-		}
-
-		if (pointerS >= 0 && pointerT >= 0 && S[pointerS] !== T[pointerT]) {
+		pointerS = nextValidChar(S, pointerS)
+		pointerT = nextValidChar(T, pointerT)
+		charS = pointerS >= 0 ? S[pointerS] : ''
+		charT = pointerT >= 0 ? T[pointerT] : ''
+		if (charS !== charT) {
 			return false
 		}
-
-		if (pointerS >= 0 !== pointerT >= 0) {
-			return false
-		}
-
 		pointerS--
 		pointerT--
 	}
