d2c43e6c08f7feed65f5b919fb51dd5bdb40b26e
Puneeth1996 <puneeth1996p@gmail.com>
2025-01-11 22:07:55 +0530

    1160. Find Words That Can Be Formed by Characters

---
+++ leet-code/hash-table-topics/1160. Find Words That Can Be Formed by Characters/1160. Find Words That Can Be Formed by Characters.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string[]} words
+ * @param {string} chars
+ * @return {number}
+ */
+var countCharacters = function (words, chars) {
+	let count = {}
+	for (let i = 0; i < chars.length; i++) {
+		let ch = chars.charAt(i)
+		if (!count[ch]) {
+			count[ch] = 1
+		} else {
+			count[ch] += 1
+		}
+	}
+	res = 0
+	for (const each_word of words) {
+		cur_word_count = {}
+		for (let i = 0; i < each_word.length; i++) {
+			let word_ch = each_word.charAt(i)
+			if (!cur_word_count[word_ch]) {
+				cur_word_count[word_ch] = 1
+			} else {
+				cur_word_count[word_ch] += 1
+			}
+		}
+		let flag = true
+		for (let i = 0; i < each_word.length; i++) {
+			c = each_word[i]
+			if (!(c in count) || cur_word_count[c] > count[c]) {
+				flag = false
+				break
+			}
+		}
+		if (flag) res += each_word.length
+	}
+	return res
+}
