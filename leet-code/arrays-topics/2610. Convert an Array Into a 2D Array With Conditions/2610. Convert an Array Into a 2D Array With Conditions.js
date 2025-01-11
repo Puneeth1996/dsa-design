29043990521b0deebf2fdd39e027e94cb29ede24
Puneeth1996 <puneeth1996p@gmail.com>
2025-01-11 22:14:02 +0530

    2610. Convert an Array Into a 2D Array With Conditions

---
+++ leet-code/arrays-topics/2610. Convert an Array Into a 2D Array With Conditions/2610. Convert an Array Into a 2D Array With Conditions.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var findMatrix = function (nums) {
+	const ans = []
+	const n = nums.length
+	const cnt = new Array(n + 1).fill(0)
+	for (const x of nums) {
+		++cnt[x]
+	}
+	for (let x = 1; x <= n; ++x) {
+		for (let j = 0; j < cnt[x]; ++j) {
+			if (ans.length <= j) {
+				ans.push([])
+			}
+			ans[j].push(x)
+		}
+	}
+	return ans
+}
