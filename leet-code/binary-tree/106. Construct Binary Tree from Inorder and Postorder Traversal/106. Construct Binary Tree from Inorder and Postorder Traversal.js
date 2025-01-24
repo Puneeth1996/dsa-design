88d7f2fee9888e14c4f80a34c40e97dca9de95df
Puneeth1996 <puneeth1996p@gmail.com>
2025-01-24 11:00:54 +0530

    106. Construct Binary Tree from Inorder and Postorder Traversal

--- leet-code/binary-tree/106. Construct Binary Tree from Inorder and Postorder Traversal/106. Construct Binary Tree from Inorder and Postorder Traversal.js
+++ leet-code/binary-tree/106. Construct Binary Tree from Inorder and Postorder Traversal/106. Construct Binary Tree from Inorder and Postorder Traversal.js
@@ -12,20 +12,31 @@
  * @return {TreeNode}
  */
 function buildTree(inorder, postorder) {
-	const n = inorder.length
-	const d = {}
-	for (let i = 0; i < n; i++) {
-		d[inorder[i]] = i
+	if (postorder.length === 0) {
+		return null
 	}
-	const dfs = (i, j, n) => {
-		if (n <= 0) {
-			return null
-		}
-		const v = postorder[j + n - 1]
-		const k = d[v]
-		const l = dfs(i, j, k - i)
-		const r = dfs(k + 1, j + k - i, n - 1 - (k - i))
-		return new TreeNode(v, l, r)
-	}
-	return dfs(0, 0, n)
+
+	// The last element in postorder traversal is the root node's value.
+	const rootValue = postorder[postorder.length - 1]
+	// Find the index of the root value in the inorder traversal.
+	const rootIndexInInorder = inorder.indexOf(rootValue)
+
+	// Construct the root node of the tree.
+	const rootNode = new TreeNode(
+		rootValue,
+		// Recursively build the left subtree from the left part of inorder and postorder arrays.
+		buildTree(
+			inorder.slice(0, rootIndexInInorder),
+			postorder.slice(0, rootIndexInInorder)
+		),
+		// Recursively build the right subtree from the right part of inorder and postorder arrays, excluding the last element of postorder.
+		buildTree(
+			inorder.slice(rootIndexInInorder + 1),
+			postorder.slice(rootIndexInInorder, postorder.length - 1)
+		)
+	)
+
+	// Return the root node of the reconstructed tree.
+	return rootNode
 }
+// Time Complexity O(n^2)
