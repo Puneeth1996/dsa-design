b324af8217fd7803358bdb28cf1b972ddb10078b
Puneeth1996 <puneeth1996p@gmail.com>
2025-03-01 22:15:37 +0530

    946. Validate Stack Sequences

---
+++ leet-code/stacks-topics/946. Validate Stack Sequences/946. Validate Stack Sequences.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} pushed
+ * @param {number[]} popped
+ * @return {boolean}
+ */
+var validateStackSequences = function (pushed, popped) {
+	const stack = []
+	let i = 0
+
+	for (const el of pushed) {
+		stack.push(el)
+		while (stack.length && stack[stack.length - 1] === popped[i]) {
+			stack.pop()
+			i++
+		}
+	}
+
+	return pushed.length === i
+}
+
+// Time: O(n) - single pass
+// Space: O(n) - for the stack
