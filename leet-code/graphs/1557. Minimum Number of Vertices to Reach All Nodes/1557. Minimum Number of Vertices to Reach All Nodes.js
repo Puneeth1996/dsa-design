dd1087f7b8fd08472f5837d0e02aa8b79a389c46
Puneeth1996 <puneeth1996p@gmail.com>
2025-03-10 23:31:37 +0530

    1557. Minimum Number of Vertices to Reach All Nodes

--- leet-code/graphs/1557. Minimum Number of Vertices to Reach All Nodes/1557. Minimum Number of Vertices to Reach All Nodes.js
+++ leet-code/graphs/1557. Minimum Number of Vertices to Reach All Nodes/1557. Minimum Number of Vertices to Reach All Nodes.js
@@ -3,17 +3,20 @@
  * @param {number[][]} edges
  * @return {number[]}
  */
-var findSmallestSetOfVertices = function(n, edges) {
-    let map = new Array(n);
-    let ans = [];
-    
-    for (let [index,value] of edges) {
-        map[value] = 1;
-    };
-    
-    for (let i = 0; i < n; i++) {
-        if (!map[i]) ans.push(i);
-    };
-    
-    return ans;
-};
+var findSmallestSetOfVertices = function (n, edges) {
+	let map = new Array(n)
+	let ans = []
+
+	for (let [index, value] of edges) {
+		map[value] = 1
+	}
+
+	for (let i = 0; i < n; i++) {
+		if (!map[i]) ans.push(i)
+	}
+
+	return ans
+}
+
+// The solution approach is about getting the vertices where there is no incomming node
+// That is in other words there is only outgoing node
