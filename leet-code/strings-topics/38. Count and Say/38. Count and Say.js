d47feb587b96d175e83b6be8a4490c97e268fe52
Puneeth1996 <puneeth1996p@gmail.com>
2025-04-18 13:11:38 +0530

    38. Count and Say

--- leet-code/strings-topics/38. Count and Say/38. Count and Say.js
+++ leet-code/strings-topics/38. Count and Say/38. Count and Say.js
@@ -3,21 +3,23 @@
  * @return {string}
  */
 var countAndSay = function (n) {
-	let res = '1'
-	for (let i = 1; i < n; i++) {
-		let temp = '',
-			count = 1
-		for (let j = 1; j < res.length; j++) {
-			if (res[j] === res[j - 1]) {
-				count++
-			} else {
-				temp += count + res[j - 1]
+	var str = '1'
+	for (var i = 1; i < n; i++) {
+		var strArray = str.split('')
+		str = ''
+		var count = 1
+		// Loop through current nth level line
+		for (var j = 0; j < strArray.length; j++) {
+			// Next digit is different
+			if (strArray[j] !== strArray[j + 1]) {
+				// Go to next non-matching digit
+				str += count + strArray[j]
 				count = 1
+			} else {
+				count++
 			}
 		}
-		temp += count + res[res.length - 1]
-		res = temp
 	}
-	return res
+	return str
 }
 // Time & Space Complexity: O(2^n)
