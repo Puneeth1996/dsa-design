59c5dd2210fad8ba9f2318caf5cab334c1c42ffb
Puneeth1996 <puneeth1996p@gmail.com>
2025-04-27 22:58:43 +0530

    3392. Count Subarrays of Length Three With a Condition

---
+++ leet-code/arrays-topics/3392. Count Subarrays of Length Three With a Condition/3392. Count Subarrays of Length Three With a Condition.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countSubarrays = function (nums) {
+	let res = 0
+	for (let i = 0; i < nums.length - 2; i++) {
+		if ((nums[i] + nums[i + 2]) * 2 === nums[i + 1]) {
+			res++
+		}
+	}
+	return res
+}
+// Time complexity: O(n)
+// Space complexity: O(1)
