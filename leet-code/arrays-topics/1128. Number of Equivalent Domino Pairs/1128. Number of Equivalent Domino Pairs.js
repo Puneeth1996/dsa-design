dfb81f6e54a16e593fb88914c88c190ff8607f2e
Puneeth1996 <puneeth1996p@gmail.com>
2025-05-04 22:28:16 +0530

    1128. Number of Equivalent Domino Pairs

---
+++ leet-code/arrays-topics/1128. Number of Equivalent Domino Pairs/1128. Number of Equivalent Domino Pairs.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} dominoes
+ * @return {number}
+ */
+var numEquivDominoPairs = function (dominoes) {
+	let count = new Array(100).fill(0),
+		res = 0
+	for (let [a, b] of dominoes) {
+		let val = a < b ? a * 10 + b : b * 10 + a
+		res += count[val]++
+	}
+	return res
+}
+// Time complexity: O(n) Space complexity: O(1)
