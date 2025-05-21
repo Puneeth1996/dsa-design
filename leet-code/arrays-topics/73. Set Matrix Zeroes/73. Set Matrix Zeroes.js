ce9981688e6e56d27607dfd8458a1e43179150af
Puneeth1996 <puneeth1996p@gmail.com>
2025-05-21 23:21:27 +0530

    73. Set Matrix Zeroes

--- leet-code/arrays-topics/73. Set Matrix Zeroes/73. Set Matrix Zeroes.js
+++ leet-code/arrays-topics/73. Set Matrix Zeroes/73. Set Matrix Zeroes.js
@@ -3,24 +3,24 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function (matrix) {
-	const inputRows = matrix.length
-	const inputCols = matrix[0].length
+	const m = matrix.length
+	const n = matrix[0].length
 
 	let firstRowZero = false
 	let firstColZero = false
 
 	// Step 1: Check if first row and col need to be zero
-	for (let i = 0; i < inputRows; i++) {
+	for (let i = 0; i < m; i++) {
 		if (matrix[i][0] === 0) firstColZero = true
 	}
 
-	for (let j = 0; j < inputCols; j++) {
+	for (let j = 0; j < n; j++) {
 		if (matrix[0][j] === 0) firstRowZero = true
 	}
 
 	// Step 2: Use first row and col to mark zeros
-	for (let i = 1; i < inputRows; i++) {
-		for (let j = 1; j < inputCols; j++) {
+	for (let i = 1; i < m; i++) {
+		for (let j = 1; j < n; j++) {
 			if (matrix[i][j] === 0) {
 				matrix[i][0] = 0
 				matrix[0][j] = 0
@@ -29,8 +29,8 @@ var setZeroes = function (matrix) {
 	}
 
 	// Step 3: Zero out marked rows and cols
-	for (let i = 1; i < inputRows; i++) {
-		for (let j = 1; j < inputCols; j++) {
+	for (let i = 1; i < m; i++) {
+		for (let j = 1; j < n; j++) {
 			if (matrix[i][0] === 0 || matrix[0][j] === 0) {
 				matrix[i][j] = 0
 			}
@@ -39,10 +39,10 @@ var setZeroes = function (matrix) {
 
 	// Step 4: Zero out first row and col if needed
 	if (firstRowZero) {
-		for (let j = 0; j < inputCols; j++) matrix[0][j] = 0
+		for (let j = 0; j < n; j++) matrix[0][j] = 0
 	}
 
 	if (firstColZero) {
-		for (let i = 0; i < inputRows; i++) matrix[i][0] = 0
+		for (let i = 0; i < m; i++) matrix[i][0] = 0
 	}
 }
