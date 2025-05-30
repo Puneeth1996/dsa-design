455fc55982c01b6ef9fa789a8bfa3a38cc86f7b9
Puneeth1996 <puneeth1996p@gmail.com>
2025-05-30 22:07:59 +0530

    2359. Find Closest Node to Given Two Nodes

--- leet-code/graphs/2359. Find Closest Node to Given Two Nodes/2359. Find Closest Node to Given Two Nodes.js
+++ leet-code/graphs/2359. Find Closest Node to Given Two Nodes/2359. Find Closest Node to Given Two Nodes.js
@@ -6,13 +6,13 @@
  */
 var closestMeetingNode = function (edges, node1, node2) {
 	function getDistances(start) {
-		const dist = new Array(edges.length).fill(-1)
+		const dist = new Array(edges.length).fill(-1) // -1 means unreachable
 		let current = start
 		let d = 0
 		while (current !== -1 && dist[current] === -1) {
 			dist[current] = d
 			d++
-			current = edges[current]
+			current = edges[current] // move to next node
 		}
 		return dist
 	}
@@ -20,12 +20,12 @@ var closestMeetingNode = function (edges, node1, node2) {
 	const dist2 = getDistances(node2)
 	let minDist = Infinity
 	let result = -1
-	for (let i = 0; i < edges.lenght; i++) {
+	for (let i = 0; i < edges.length; i++) {
 		if (dist1[i] !== -1 && dist2[i] !== -1) {
-			const maxDist = Math.mmax(dist1[i], dist2[i])
+			const maxDist = Math.max(dist1[i], dist2[i])
 			if (maxDist < minDist) {
-				result = i
 				minDist = maxDist
+				result = i
 			}
 		}
 	}
