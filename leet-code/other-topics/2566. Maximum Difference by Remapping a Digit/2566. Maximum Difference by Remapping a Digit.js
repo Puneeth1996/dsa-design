3b2b7c479a722898250bdff2390d3d1b01f6e5db
Puneeth1996 <puneeth1996p@gmail.com>
2025-06-14 23:14:31 +0530

    2566. Maximum Difference by Remapping a Digit

--- leet-code/other-topics/2566. Maximum Difference by Remapping a Digit/2566. Maximum Difference by Remapping a Digit.js
+++ leet-code/other-topics/2566. Maximum Difference by Remapping a Digit/2566. Maximum Difference by Remapping a Digit.js
@@ -4,29 +4,14 @@
  */
 var minMaxDifference = function (num) {
 	const str = num.toString()
+	const firstNon9 = str[str.search(/[0-8]/)] ?? -1
+	const firstNon0 = str[str.search(/[1-9]/)] ?? -1
 
-	// Maximize: replace the first digit not '9' with '9'
-	let maxStr = str
-	for (let i = 0; i < str.length; i++) {
-		if (str[i] !== '9') {
-			maxStr = str.split(str[i]).join('9')
-			break
-		}
-	}
+	const max =
+		firstNon9 + 1 ? +str.replace(new RegExp(firstNon9, 'g'), '9') : num
+	const min =
+		firstNon0 + 1 ? +str.replace(new RegExp(firstNon0, 'g'), '0') : num
 
-	// Minimize: replace the first digit not '1' with '0' or '1' (avoid leading 0)
-	let minStr = str
-	if (str[0] !== '1') {
-		minStr = str.split(str[0]).join('1')
-	} else {
-		for (let i = 1; i < str.length; i++) {
-			if (str[i] !== '0' && str[i] !== '1') {
-				minStr = str.split(str[i]).join('0')
-				break
-			}
-		}
-	}
-
-	return parseInt(maxStr) - parseInt(minStr)
+	return max - min
 }
 // Time and space Complexity: O(n)
