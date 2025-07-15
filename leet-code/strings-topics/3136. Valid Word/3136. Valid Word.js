b73c1f1f7f74205073d26a9af78e03f7c49d4729
Puneeth1996 <puneeth1996p@gmail.com>
2025-07-15 20:15:46 +0530

    3136. Valid Word

--- leet-code/strings-topics/3136. Valid Word/3136. Valid Word.js
+++ leet-code/strings-topics/3136. Valid Word/3136. Valid Word.js
@@ -2,19 +2,22 @@
  * @param {string} word
  * @return {boolean}
  */
-var isValid = function(word) {
-      if (word.length < 3) return false;
+var isValid = function (word) {
+	if (word.length < 3) return false
 
-  const vowelsSet = new Set('aeiouAEIOU'.split(''));
-  let v = 0, c = 0;
+	const vowelsSet = new Set('aeiouAEIOU'.split(''))
+	let v = 0,
+		c = 0
 
-  for (const ch of word) {
-    if (/\d/.test(ch)) continue;
-    if (/[a-zA-Z]/.test(ch)) {
-      vowelsSet.has(ch) ? ++v : ++c;
-    } else {
-      return false;
-    }
-  }
-  return v > 0 && c > 0;
-};
+	for (const ch of word) {
+		if (/\d/.test(ch)) continue
+		if (/[a-zA-Z]/.test(ch)) {
+			vowelsSet.has(ch) ? ++v : ++c
+		} else {
+			return false
+		}
+	}
+	return v > 0 && c > 0
+}
+// Time	O(n)	Single pass over n characters.
+// Space	O(1)	Only constant counters and the fixed vowel set.
