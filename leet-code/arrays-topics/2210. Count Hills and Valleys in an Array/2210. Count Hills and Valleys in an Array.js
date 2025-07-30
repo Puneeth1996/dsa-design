f38efc5ecd07ff167743c10162bacc9f084f83e1
Puneeth1996 <puneeth1996p@gmail.com>
2025-07-31 01:27:47 +0530

    2210. Count Hills and Valleys in an Array

---
+++ leet-code/arrays-topics/2210. Count Hills and Valleys in an Array/2210. Count Hills and Valleys in an Array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countHillValley = function (nums) {
+	let count = 0
+	let left = 0
+	for (let i = 1; i < nums.length - 1; i++) {
+		if (nums[i] !== nums[i + 1]) {
+			if (
+				(nums[i] > nums[left] && nums[i] > nums[i + 1]) ||
+				(nums[i] < nums[left] && nums[i] < nums[i + 1])
+			) {
+				count++
+			}
+			left = i
+		}
+	}
+	return count
+}
+// Time Complexity: O(n)
+// Space Complexity: O(1)
