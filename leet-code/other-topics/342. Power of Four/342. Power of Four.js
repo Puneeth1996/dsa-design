a62bbe6330964c4b78ed2604e8931e305441a9ce
Puneeth1996 <puneeth1996p@gmail.com>
2025-08-15 23:25:34 +0530

    342. Power of Four

--- leet-code/other-topics/342. Power of Four/342. Power of Four.js
+++ leet-code/other-topics/342. Power of Four/342. Power of Four.js
@@ -3,13 +3,11 @@
  * @return {boolean}
  */
 var isPowerOfFour = function (num) {
-	if (num <= 0) {
-		return false
-	}
+	if (num <= 0) return false
+
 	while (num > 1) {
-		if (num % 4 != 0) {
-			return false
-		}
+		if (num % 4 != 0) return false
+
 		num = num / 4
 	}
 	return true
